542cfa264fc745003dd8503c368af586d8179489
FujiwaraTakeshi <241056@std.hi-joho.ac.jp>
2025-07-09 10:40:21 +0900

    2025/07/09, 1限

---
+++ src/Goblin.java
@@ -0,0 +1,11 @@
+public class Goblin extends Monster {
+    public Goblin(char suffix, int hp) {
+        super(suffix, hp);
+    }
+
+    public void attack(Creature target) {
+        int damage = 8;
+        System.out.println("ゴブリン" + getSuffix() + "はナイフで切りつけた！" + target.getName() + "に" + damage + "のダメージを与えた！");
+        target.setHp(target.getHp() - damage);
+    }
+}
---
+++ src/Monster.java
@@ -0,0 +1,37 @@
+public abstract class Monster implements Creature {
+    private String name;
+    private int hp;
+    private char suffix;
+
+    public Monster(char suffix, int hp) {
+        if(hp < 0) {
+            throw new IllegalArgumentException("初期設定に誤りがあるため、キャラクターを作成できませんでした");
+        }
+        this.suffix = suffix;
+        this.hp = hp;
+    }
+
+    public final boolean isAlive() {
+        return this.hp > 0;
+    }
+
+    public void showStatus() {
+        System.out.println(this.name + ": HP " + this.hp);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getHp() {
+        return this.hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = Math.max(hp, 0);
+    }
+
+    public char getSuffix() {
+        return suffix;
+    }
+}
